733cf26fa6648f8108667de0daaa36ebbb8a23c4
Nanda Hady Mulya <nandahadymulyaa@gmail.com>
2022-08-08 09:12:11 +0700

    feat: notif success uploaded

--- blr/views.py
+++ blr/views.py
@@ -1,5 +1,5 @@
 import http
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib import messages
 from .forms import UploadFileForm
@@ -10,8 +10,8 @@ def index(request):
       form = UploadFileForm(request.POST, request.FILES)
       if form.is_valid():
         handle_uploaded_file(request.FILES['file'])
-        messages.success(request, 'File uploaded successfully')
-        return HttpResponse(status=http.HTTPStatus.OK)
+        message = messages.success(request, 'File uploaded successfully')
+        return HttpResponseRedirect('/', {message})
     else:
       form = UploadFileForm() 
       return render(request, 'views/index.html',{'form':form})
